ef9bee8488be3da0e497d9f3468596a012155714
Eduardo Dias <edurdias@gmail.com>
2024-10-21 22:47:35 -0400

    added replay scenario for simple graph tests

--- tests/examples/graph/test_simple_graph.py
+++ tests/examples/graph/test_simple_graph.py
@@ -8,6 +8,17 @@ def test_should_succeed():
         ctx.finished and ctx.succeeded
     ), "The workflow should have been completed successfully."
     assert ctx.output == "Hello, Joe"
+    
+def test_should_replay():
+    first_ctx = simple_graph.run("Joe")
+    assert (
+        first_ctx.finished and first_ctx.succeeded
+    ), "The workflow should have been completed successfully."
+    assert first_ctx.output == "Hello, Joe"
+
+    second_ctx = simple_graph.run("Joe", execution_id=first_ctx.execution_id)
+    assert first_ctx.execution_id == second_ctx.execution_id
+    assert first_ctx.output == second_ctx.output
 
 
 def test_should_fail():
--- tests/examples/test_hello_world.py
+++ tests/examples/test_hello_world.py
@@ -12,11 +12,14 @@ def test_should_succeed():
 
 def test_should_replay():
     first_ctx = hello_world.run("Joe")
+    assert (
+        first_ctx.finished and first_ctx.succeeded
+    ), "The workflow should have been completed successfully."
     assert first_ctx.output == "Hello, Joe"
 
     second_ctx = hello_world.run("Joe", execution_id=first_ctx.execution_id)
+    assert first_ctx.execution_id == second_ctx.execution_id
     assert first_ctx.output == second_ctx.output
-    assert first_ctx.events[-1] == second_ctx.events[-1]
 
 
 def test_should_fail():
