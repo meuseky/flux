8d518c4220ddeaa9b71e51cd894294f69abe01e9
Eduardo Dias <edurdias@gmail.com>
2024-11-07 22:10:19 -0500

    removed module catalog

--- flux/catalogs.py
+++ flux/catalogs.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import sys
 from abc import ABC
 from abc import abstractmethod
 from importlib import import_module
@@ -92,28 +91,3 @@ class SQLiteWorkflowCatalog(WorkflowCatalog, SQLiteRepository):
             module = util.module_from_spec(spec)
             spec.loader.exec_module(module)
         return module
-
-
-class ModuleWorkflowCatalog(WorkflowCatalog):
-    def __init__(self, options: dict[str, Any] | None = None):
-        options = options or {}
-        if "module" in options:
-            self._module = import_module(options["module"])
-        elif "path" in options:
-            path = options["path"]
-            spec = util.spec_from_file_location("workflow_module", path)
-            if spec is None or spec.loader is None:
-                raise ImportError(f"Cannot find module at {path}.")
-            self._module = util.module_from_spec(spec)
-            spec.loader.exec_module(self._module)
-        else:
-            self._module = sys.modules["__main__"]
-
-    def get(self, name: str) -> decorators.workflow:
-        workflow = getattr(self._module, name) if hasattr(self._module, name) else None
-        if not workflow or not decorators.workflow.is_workflow(workflow):
-            raise WorkflowNotFoundError(name, self._module.__name__)
-        return workflow
-
-    def save(self, workflow: decorators.workflow):  # pragma: no cover
-        raise NotImplementedError()
